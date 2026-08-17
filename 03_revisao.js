fdade62bb597641ac042eecb812ca1e5a65a6577
Julia-C0utinh0 <juliacoutinhoalm@gmail.com>
2026-08-17 14:48:04 -0300

    Revisão e acumulo de variáveis

---
+++ 03_revisao.js
@@ -0,0 +1,39 @@
+/*
+let contas= 1200.00
+let cond= 250.59
+let int= 85.99
+let total= contas+cond+int
+
+console.log("O total das contas é"+ total)
+*/
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+let nom= "Catapimbas"
+let sobrenom= "De Souza"
+let nom_completo= nom +"  "+sobrenom
+
+console.log( "Meu nome é " + nom + " e meu sobrenome é "+ sobrenom + " .Junto, meu nome fica: " + nom_completo)
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+let salario= 2000
+
+console.log( "Meu salário é R$" + salario)
+
+let comis= 100
+salario= comis+salario
+
+console.log( "Meu salário é R$" + salario)
+
+let adinot= 200
+salario= adinot+salario
+
+console.log( "Meu salário é R$" + salario)
+
+/*
+comentario 
+multiplas linhas 
+lalalala
+adoro batata doce 
+explodir o turcomenistão 
+*/
